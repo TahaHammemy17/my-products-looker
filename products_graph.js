bb3f2465c6d230dee45efe8e03db1a667ecfab8a
Taha Hammemy <hammemy.taha@gmail.com>
2023-10-11 16:37:01 +0200

    test-commit

--- products_graph.js
+++ products_graph.js
@@ -27,14 +27,14 @@ const visObject = {
                     label: 'Top 5 Product Category',
                     data: actual_data,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 99, 88, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
                         'rgba(153, 102, 255, 0.2)'
                     ],
                     borderColor: [
-                        'rgba(255, 99, 132, 1)',
+                        'rgba(255, 99, 88, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
